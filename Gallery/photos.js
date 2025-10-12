28434b4b34c06326f9440578556e6773b1e58638
Drew Burrier <drew_burrier@yahoo.com>
2025-10-12 12:16:51 -1000

    gallery update
    photos and copy updates

--- Gallery/photos.js
+++ Gallery/photos.js
@@ -1,6 +1,6 @@
 const photoData = [
     {
-        "filename": "IMG_0038.jpg",
+        "filename": "IMG_0038.jpeg",
         "datetime": "2023:09:04 15:53:04",
         "gps": {
             "latitude": 21.576625,
@@ -40,7 +40,15 @@ const photoData = [
         }
     },
     {
-        "filename": "IMG_3712.jpg",
+        "filename": "IMG_2709.jpeg",
+        "datetime": "2024:04:13 09:36:03",
+        "gps": {
+            "latitude": 21.610572222222224,
+            "longitude": -158.1075138888889
+        }
+    },
+    {
+        "filename": "IMG_3712.jpeg",
         "datetime": "2024:06:08 14:11:05",
         "gps": {
             "latitude": 21.505133333333333,
@@ -48,11 +56,123 @@ const photoData = [
         }
     },
     {
-        "filename": "IMG_5613.jpg",
+        "filename": "IMG_3821.jpeg",
+        "datetime": "2024:06:18 16:01:09",
+        "gps": {
+            "latitude": 21.27259722222222,
+            "longitude": -157.81634444444447
+        }
+    },
+    {
+        "filename": "IMG_4025.jpeg",
+        "datetime": "2024:06:29 11:50:25",
+        "gps": {
+            "latitude": 21.316458333333333,
+            "longitude": -157.664825
+        }
+    },
+    {
+        "filename": "IMG_4039.jpeg",
+        "datetime": "2024:06:29 16:58:35",
+        "gps": {
+            "latitude": 21.34188333333333,
+            "longitude": -157.7995611111111
+        }
+    },
+    {
+        "filename": "IMG_4228.jpeg",
+        "datetime": "2024:07:02 16:35:14",
+        "gps": {
+            "latitude": 21.265833333333333,
+            "longitude": -157.82031388888888
+        }
+    },
+    {
+        "filename": "IMG_4512.jpeg",
+        "datetime": "2024:07:14 15:02:37",
+        "gps": {
+            "latitude": 21.346241666666664,
+            "longitude": -157.82139722222223
+        }
+    },
+    {
+        "filename": "IMG_5613.jpeg",
         "datetime": "2024:09:11 06:26:18",
         "gps": {
             "latitude": 21.28555277777778,
             "longitude": -157.67234722222221
+        }
+    },
+    {
+        "filename": "IMG_0023.jpeg",
+        "datetime": "2025:06:01 09:51:00",
+        "gps": {
+            "latitude": 21.465297222222222,
+            "longitude": -157.82780555555556
+        }
+    },
+    {
+        "filename": "IMG_0324.jpeg",
+        "datetime": "2025:06:28 19:15:01",
+        "gps": {
+            "latitude": 21.57443333333333,
+            "longitude": -158.27943333333334
+        }
+    },
+    {
+        "filename": "IMG_1212.jpeg",
+        "datetime": "2025:08:16 08:19:09",
+        "gps": {
+            "latitude": 21.494658333333334,
+            "longitude": -158.23631388888887
+        }
+    },
+    {
+        "filename": "IMG_1229.jpeg",
+        "datetime": "2025:08:16 09:02:43",
+        "gps": {
+            "latitude": 21.527338888888888,
+            "longitude": -158.23384166666665
+        }
+    },
+    {
+        "filename": "IMG_1734.jpeg",
+        "datetime": "2025:09:07 08:33:44",
+        "gps": {
+            "latitude": 21.56758333333333,
+            "longitude": -157.86646944444445
+        }
+    },
+    {
+        "filename": "IMG_1752.jpeg",
+        "datetime": "2025:09:07 10:29:51",
+        "gps": {
+            "latitude": 21.65138888888889,
+            "longitude": -157.9129638888889
+        }
+    },
+    {
+        "filename": "IMG_2042.jpeg",
+        "datetime": "2025:10:03 09:31:23",
+        "gps": {
+            "latitude": 21.47888611111111,
+            "longitude": -157.83523611111113
+        }
+    },
+    {
+        "filename": "IMG_2054.jpeg",
+        "datetime": "2025:10:03 10:20:20",
+        "gps": {
+            "latitude": 21.508122222222223,
+            "longitude": -157.82876666666667
+        }
+    },
+    {
+        "filename": "IMG_2168.jpeg",
+        "datetime": "2025:10:11 17:57:46",
+        "gps": {
+            "latitude": 21.553208333333334,
+            "longitude": -158.2467333333333
         }
     }
 ];
